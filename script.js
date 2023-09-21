58a1ede9365aebce61c64f4e6150368c6cb2da21
Josh <143891382+joshwhite88@users.noreply.github.com>
2023-09-21 17:15:43 -0500

    added ability to randomize knock knock joke

---
+++ script.js
@@ -0,0 +1,10 @@
+const knockKnockJokes = [ ['Owls', 'They sure do'], ['Adorable', "A-doorbell doesn't work, that's why I knocked"],
+['Daisy', 'Dey see me rollin, dey hatin'], ['Kenya', 'Kenya open the door']];
+
+let jokeIndex = Math.floor(Math.random() * 4);
+
+console.log('Knock knock.');
+console.log("Who's there?");
+console.log(knockKnockJokes[jokeIndex][0]);
+console.log(knockKnockJokes[jokeIndex][0] + ' who?');
+console.log(knockKnockJokes[jokeIndex][1]);
