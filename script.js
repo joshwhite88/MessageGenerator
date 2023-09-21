e37190312726a62bec7db35d6929bd67a49b2e82
Josh <143891382+joshwhite88@users.noreply.github.com>
2023-09-21 17:22:21 -0500

    added another joke

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const knockKnockJokes = [ ['Owls', 'They sure do!'], ['Adorable', "A-doorbell doesn't work, that's why I knocked!"],
-['Daisy', 'Dey see me rollin, dey hatin...'], ['Kenya', 'Kenya open the door?']];
+['Daisy', 'Dey see me rollin, dey hatin...'], ['Kenya', 'Kenya open the door?'], ['Lettuce', 'Lettuce in!']];
 
-let jokeIndex = Math.floor(Math.random() * 4);
+let jokeIndex = Math.floor(Math.random() * 5);
 
 console.log('Knock knock.');
 console.log("Who's there?");
